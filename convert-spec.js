a7a6c4750c3722dfd89c285b7a2ef4b4c34d40dd
Nick Meldrum <nick@nickmeldrum.com>
2017-01-23 12:08:00 +0000

    adds base64 tests

--- convert-spec.js
+++ convert-spec.js
@@ -5,19 +5,43 @@ const expect = chai.expect
 
 const convert = require('./convert.js')
 
+const input = 'a test'
+const args = {
+    encoding: 'utf8'
+}
+
 describe('convert', function() {
     describe('rot13', function() {
         it('run once is the rotated text', function() {
-            const input = 'a test'
-            const once = convert.rot13({}, input)
+            const once = convert.rot13(args, input)
             expect(once).to.equal('n grfg')
         })
 
         it('run twice returns original', function() {
-            const input = 'a test'
-            const once = convert.rot13({}, input)
-            const twice = convert.rot13({}, once)
+            const once = convert.rot13(args, input)
+            const twice = convert.rot13(args, once)
             expect(twice).to.equal(input)
+        })
+    })
+
+    describe('base64', function() {
+        it('encoding then decoding returns original', function() {
+            const encoded = convert.base64encode(args, input)
+            const decoded = convert.base64decode(args, encoded)
+
+            expect(decoded).to.equal(input)
+        })
+
+        it('encoding is different to original', function() {
+            const encoded = convert.base64encode(args, input)
+
+            expect(encoded).to.not.equal(input)
+        })
+
+        it('encoding is correctly encoded', function() {
+            const encoded = convert.base64encode(args, input)
+
+            expect(encoded).to.equal('YSB0ZXN0')
         })
     })
 })
